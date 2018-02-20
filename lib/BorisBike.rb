af30150eb6aad6a24d80fba9290e7ad3a3f09160
Ajibaji <persiancookbook@gmail.com>
2018-02-20 16:53:18 +0000

    added DOCK

--- lib/BorisBike.rb
+++ lib/BorisBike.rb
@@ -6,6 +6,10 @@ class DockingStation
   def release_bike
     @bike = Bike.new
   end
+
+  def dock_bike
+    "DOCKED!"
+  end
 end
 
 class Bike
--- spec/bb_spec.rb
+++ spec/bb_spec.rb
@@ -2,4 +2,7 @@ require 'BorisBike'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  
+  it { is_expected.to respond_to(:dock_bike) }
 end
+
